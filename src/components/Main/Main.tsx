9692394dca7775056b15adfb040c89ec7fff5784
Maksym Nemera <nickmitchell@ukr.net>
2023-09-08 16:41:49 +0300

    add redux/toolkit to template

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,23 @@
 import { FC } from 'react';
 import './Main.scss';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as counterAction } from '../../features/counter/counterSlice';
 
 export const Main: FC = () => {
-  return <main className='Main'>MAIN</main>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const increment = () => dispatch(counterAction.increment());
+  const decrement = () => dispatch(counterAction.decrement());
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const incrementByAmount = (value: number) =>
+    dispatch(counterAction.incrementByAmount(value));
+
+  return (
+    <main className='Main'>
+      <button onClick={increment}>+</button>
+      {count}
+      <button onClick={decrement}>-</button>
+    </main>
+  );
 };
